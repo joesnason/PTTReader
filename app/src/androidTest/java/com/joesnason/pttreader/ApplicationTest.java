43a926c8c75a67cd92c9909f772d37de61957148
joesnason <joesnason@gmail.com>
2016-04-11 15:51:28 +0800

    change project name

--- app/src/androidTest/java/com/joesnason/pttreader/ApplicationTest.java
+++ app/src/androidTest/java/com/joesnason/pttreader/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.joesnason.bbsreader;
+package com.joesnason.pttreader;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/joesnason/pttreader/MainActivity.java
+++ app/src/main/java/com/joesnason/pttreader/MainActivity.java
@@ -1,4 +1,4 @@
-package com.joesnason.bbsreader;
+package com.joesnason.pttreader;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -65,9 +65,9 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(com.joesnason.pttreader.R.layout.activity_main);
 
-        Button connbtn = (Button) findViewById(R.id.connButton);
+        Button connbtn = (Button) findViewById(com.joesnason.pttreader.R.id.connButton);
         connbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -76,7 +76,7 @@ public class MainActivity extends Activity {
             }
         });
 
-        contentView = (TextView) findViewById(R.id.contentView);
+        contentView = (TextView) findViewById(com.joesnason.pttreader.R.id.contentView);
 
         UIhandler = new Handler() {
 
@@ -112,7 +112,7 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(com.joesnason.pttreader.R.menu.menu_main, menu);
         return true;
     }
 
@@ -124,7 +124,7 @@ public class MainActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == com.joesnason.pttreader.R.id.action_settings) {
             return true;
         }
 
